870b26b521b6b95a77a5c05d7bf16799e1a0314a
ASUALT <qvantum.gamer@gmail.com>
2025-02-18 15:10:07 +0200

    New file structure and Express server setup

---
+++ src/server.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on http://localhost:${PORT}`); 
+}); 
