45bbd1be20d5471ddcf52eb9b31d26a4640525ed
Anton <toha88614@gmail.com>
2025-03-12 15:41:16 +0200

    Added slider and lesson schedule table to the homepage Created "Ogoloshennya" page Created photo gallery with adaptive layout SEO optimization for the homepage and photo gallery

---
+++ public/styles/main/js/main.js
@@ -0,0 +1,10 @@
+//Home
+import './pages/home/section_table/toggleDescription.js'
+import './pages/home/section_table/slider.js'
+
+
+//Advertisement
+import './pages/advertisement/section_advertisement-new/modal-window.js'
+
+//Gallery
+import "./pages/gallary/button.js"
--- src/server.mjs
+++ src/server.mjs
@@ -3,17 +3,30 @@ import dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-const allowedPages = ["index", "advertisement"] // add a new page if needed
+const allowedPages = ["index", "advertisement", "gallery"];
 
 const app = express();
 
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "../public"));
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
 app.use(express.static(path.join(__dirname, "../public")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/html/index.html"));
 });
@@ -22,11 +35,13 @@ app.get("/:page", (req, res) => {
     const page = req.params.page;
     const filePath = path.join(__dirname, `../public/html/${page}.html`);
 
-    // Check if the page exists in the allowed list and the file exists
-    if (allowedPages.includes(page) && fs.existsSync(filePath)) res.sendFile(filePath);
-    else res.status(404).send("Page not found");
+    if (allowedPages.includes(page) && fs.existsSync(filePath)) {
+        res.sendFile(filePath);
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on http://localhost:${PORT}`); 
-}); 
+    console.log(`Running on http://localhost:${PORT}`);
+});
