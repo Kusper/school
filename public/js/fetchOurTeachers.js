75cd8a6dbaa74be84465b170f05a27e1ea242634
ASUALT <qvantum.gamer@gmail.com>
2025-03-19 11:43:00 +0200

    Load teacher to the page

--- public/js/fetchOurTeachers.js
+++ public/js/fetchOurTeachers.js
@@ -1,14 +1,23 @@
+const teachersBlock = document.querySelector(".section_advertisement-active_list");
+
+
 fetch("/api/our_teachers")
     .then( res => res.json())
     .then( data => {
-        const photoBlock = document.querySelector(".content");
         data.forEach(item => {
-            photoBlock.innerHTML += `<div style="background-color:#ccc; font-size:10px; margin:10px;">
-                <img src="./${item.photo_path}" alt="Teacher image">
-                <p>${item.full_name}</p>
-                <p>${item.subject}</p>
-                <p>${item.description}</p>
-            </div>`;
+            teachersBlock.innerHTML += `<article class="section_advertisement-active_list-item" itemscope itemtype="https://schema.org/Person">
+                    <div class="section_advertisement-active_list-item-img">
+                        <img
+                                class="section_advertisement-active_list-item-img_image"
+                                src="../${item.picture_path}"
+                                alt="Портрет викладача ${item.full_name}"
+                                loading="lazy"
+                                itemprop="image">
+                    </div>
+                    <div class="section_advertisement-active_list-item-text">
+                        <h2 class="section_advertisement-active_list-item-text-name" itemprop="name">${item.full_name}</h2>
+                    </div>
+                </article>`;
         });
     })
     .catch( error => console.error("Error fetching our_techers:", error))
